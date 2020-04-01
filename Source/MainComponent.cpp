aed41c7a636ac8172f5971bdf094e3a3f3b48020
zackffww <zackfw@gmail.com>
2020-03-31 23:38:57 -0500

    changed text position

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -26,7 +26,7 @@ void MainComponent::paint (Graphics& g)
 
     g.setFont (Font (33.0f));
     g.setColour (Colours::green);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centredLeft, true);
 }
 
 void MainComponent::resized()
